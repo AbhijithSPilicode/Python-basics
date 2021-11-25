e8ad17ce18b6facf36455c9e454e2d690a9c6134
Abhijith S <93380590+AbhijithSPilicode@users.noreply.github.com>
2021-11-25 13:48:57 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,20 +1,11 @@
-Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 18:46:30) [MSC v.1929 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+
 print("Hello")
-Hello
 print('hello')
-hello
 type("Hello")
-<class 'str'>
 type("print")
-<class 'str'>
 type(print)
-<class 'builtin_function_or_method'>
 print(12)
-12
 type(12)
-<class 'int'>
 type(-12)
-<class 'int'>
 type(4.12)
-<class 'float'>
+
