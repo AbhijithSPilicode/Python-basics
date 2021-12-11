7443707d1ccc2fc67d7740bf8f511444bd6ce085
Abhijith S <93380590+AbhijithSPilicode@users.noreply.github.com>
2021-12-11 15:49:05 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,21 +1,6 @@
-Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 18:46:30) [MSC v.1929 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-print("a b c d")
-a b c d
-print("\n a \n b \n c \n d")
 
- a 
- b 
- c 
- d
-printf('today\'s class')
-Traceback (most recent call last):
-  File "<pyshell#3>", line 1, in <module>
-    printf('today\'s class')
-NameError: name 'printf' is not defined. Did you mean: 'print'?
-print('today\'s class)
-      
-SyntaxError: unterminated string literal (detected at line 1)
+print("a b c d")
+print("\n a \n b \n c \n d")
 print('today\'s class')
       
 today's class
